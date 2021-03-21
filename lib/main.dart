c24d5a19db3660d2f695d471abb244f766480ad3
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-21 13:49:54 +0100

    Configure Amplify Authentication at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'helpers/amplify.dart';
 import 'pages/login.dart';
 
 void main() => runApp(HerbariaApp());
 
 class HerbariaApp extends StatelessWidget {
-  // This widget is the root of your application.
+  HerbariaApp() {
+    // Initialize Amplify library to provide AWS
+    // functionality such as DataStorage, GraphQL API and Auth
+    WidgetsFlutterBinding.ensureInitialized();
+    configureAmplify();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Herbaria',
       home: LoginPage(),
     );
   }
