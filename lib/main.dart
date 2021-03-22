703838df983b08e0af606faaee75a317252c0bb7
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-22 17:27:28 +0100

    Add settings.dart to share Logger and Amplify instances across the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
-import 'helpers/amplify.dart';
 import 'pages/login.dart';
+import 'helpers/amplify.dart';
+import 'settings.dart' as Herbaria;
 
 void main() => runApp(HerbariaApp());
 
 class HerbariaApp extends StatelessWidget {
   HerbariaApp() {
-    // Initialize Amplify library to provide AWS
-    // functionality such as DataStorage, GraphQL API and Auth
+    // Calling this method is required when work is executed before calling `runApp()`
     WidgetsFlutterBinding.ensureInitialized();
-    configureAmplify();
+
+    // Initialize the logger so that it can be attached to the entire application
+    Herbaria.logger = Logger();
+
+    // Initialize AmplifyController that manages any interaction with Amplify
+    Herbaria.amplifyController = AmplifyController(amplify: Amplify);
+    Herbaria.amplifyController.configure();
   }
 
   @override
