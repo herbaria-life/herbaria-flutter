f4007d10132f1a2ce25e8c290f7b8996d5855500
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-22 18:04:52 +0100

    Implement the logic to switch between HomePage and LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,39 @@ import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
+import 'pages/home.dart';
 import 'pages/login.dart';
 import 'helpers/amplify.dart';
 import 'settings.dart' as Herbaria;
 
-void main() => runApp(HerbariaApp());
+void main() async {
+  // Calling this method is required when work is executed before calling `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the logger so that it can be attached to the entire application
+  Herbaria.logger = Logger();
+
+  // Initialize AmplifyController that manages any interaction with Amplify
+  Herbaria.amplifyController = AmplifyController(amplify: Amplify);
+
+  // Configure Amplify and check if the login process has been executed in the past
+  await Herbaria.amplifyController.configure();
+  bool isLoggedIn = await Herbaria.amplifyController.isLoggedIn();
+
+  return runApp(HerbariaApp(isLoggedIn: isLoggedIn));
+}
 
 class HerbariaApp extends StatelessWidget {
-  HerbariaApp() {
-    // Calling this method is required when work is executed before calling `runApp()`
-    WidgetsFlutterBinding.ensureInitialized();
-
-    // Initialize the logger so that it can be attached to the entire application
-    Herbaria.logger = Logger();
-
-    // Initialize AmplifyController that manages any interaction with Amplify
-    Herbaria.amplifyController = AmplifyController(amplify: Amplify);
-    Herbaria.amplifyController.configure();
-  }
+  HerbariaApp({@required this.isLoggedIn});
+  final bool isLoggedIn;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/login',
+      initialRoute: isLoggedIn ? '/home' : '/login',
       routes: {
         '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
       },
     );
   }
